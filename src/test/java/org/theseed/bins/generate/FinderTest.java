bb8028c44c12a29ca7a0c6bd5581e652108d7d18
Bruce Parrello <bparrello@figresearch.com>
2022-12-12 03:03:36 -0600

    ProteinFinder tests.

--- src/test/java/org/theseed/bins/generate/FinderTest.java
+++ src/test/java/org/theseed/bins/generate/FinderTest.java
@@ -2,12 +2,10 @@ package org.theseed.bins.generate;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,34 +16,57 @@ public class FinderTest {
     /** logging facility */
     protected static Logger log = LoggerFactory.getLogger(FinderTest.class);
 
+    /**
+     * Perform a full-scale test on the finder.  This is only possible if FINDER_PATH has been set to point to
+     * a directory with finder files in it.  The finder files are huge and can't be stored in GitHub.
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
     @Test
-    public void testFinding() throws IOException, InterruptedException {
-        File testDir = new File("data", "Finder");
-        var finder = new ProteinFinder(testDir);
-        File dnaFile1 = new File("data", "BigSample.fasta");
-        var rolesFound = finder.findSeedProteins(dnaFile1);
-        assertThat(rolesFound.size(), greaterThan(0));
-        var locList = rolesFound.get("PhenTrnaSyntAlph");
-        assertThat(locList.size(), equalTo(2));
-        List<String> nodes = locList.stream().map(x -> x.getContigId()).collect(Collectors.toList());
-        assertThat(nodes, containsInAnyOrder("NODE_12_length_174929_cov_82.7452_ID_1122656",
-                "NODE_36_length_51523_cov_936.232_ID_1122704"));
-        File dnaFile2 = new File("data", "Magnetoc.fasta");
-        rolesFound = finder.findSeedProteins(dnaFile2);
-        assertThat(rolesFound.size(), greaterThan(0));
-        for (var roleEntry : rolesFound.entrySet()) {
-            locList = roleEntry.getValue();
-           final int n = locList.size();
-           log.info("{} locations found for role {}", n, roleEntry.getKey());
-           int i = 0;
-           while (i < n) {
-               int i1 = i + 10;
-               if (i1 > n) i1 = n;
-               var subList = locList.subList(i, i1);
-               log.info("     " + subList.stream().map(x -> x.toSproutString()).collect(Collectors.joining(", ")));
-               i = i1;
-           }
+    public void bigFindingTest() throws IOException, InterruptedException {
+        String finderDirName = System.getenv("FINDER_PATH");
+        if (! StringUtils.isBlank(finderDirName)) {
+            File finderDir = new File(finderDirName);
+            File testFile = new File(finderDir, "PhenTrnaSyntAlph.fna");
+            if (testFile.exists()) {
+                log.info("Performing finder DNA species assignment test.");
+                // Find the seed proteins in BigSample.
+                var finder = new ProteinFinder(finderDir);
+                File dnaFile1 = new File("data", "BigSample.fasta");
+                var rolesFound = finder.findSeedProteins(dnaFile1);
+                // Test saving and loading the protein map.
+                File protFile = new File("data", "rolesFound.ser");
+                ProteinFinder.saveSeedProteins(rolesFound, protFile);
+                var rolesFoundLoaded = ProteinFinder.loadSeedProteins(protFile);
+                assertThat(rolesFoundLoaded.size(), equalTo(rolesFound.size()));
+                for (var roleInfo : rolesFound.entrySet()) {
+                    String roleId = roleInfo.getKey();
+                    var locListReal = roleInfo.getValue();
+                    var locListLoaded = rolesFoundLoaded.get(roleId);
+                    assertThat(roleId, locListLoaded, not(nullValue()));
+                    assertThat(roleId, locListLoaded.size(), equalTo(locListReal.size()));
+                    for (var loc : locListLoaded) {
+                        assertThat(roleId + " " + loc.toString(), locListReal, hasItem(loc));
+                    }
+                }
+                // Now do species assignment.
+                var hitsFound = finder.findRefGenomes(rolesFound, dnaFile1);
+                // Test saving and loading the dna map.
+                File dnaFile = new File("data", "hitsFound.ser");
+                ProteinFinder.saveRefGenomes(hitsFound, dnaFile);
+                var hitsFoundLoaded = ProteinFinder.loadRefGenomes(dnaFile);
+                assertThat(hitsFoundLoaded.size(), equalTo(hitsFound.size()));
+                for (var hitFoundInfo : hitsFound.entrySet()) {
+                    String contigId = hitFoundInfo.getKey();
+                    var hitLoaded = hitsFoundLoaded.get(contigId);
+                    assertThat(contigId, hitLoaded, not(nullValue()));
+                    assertThat(contigId, hitLoaded.isClone(hitFoundInfo.getValue()));
+                }
+            }
+
         }
+
     }
 
 }
